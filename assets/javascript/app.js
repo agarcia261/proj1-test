824a9e3b0da0614bf55e81165ea285bd00fd1b86
Angel <agarcia261@gmail.com>
2018-11-15 21:13:44 -0500

    test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,6 +12,35 @@ $(document).ready(function(){
     //     best:20
     // }
 
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+
+          console.log(displayName);
+          console.log(email);
+
+          console.log(emailVerified);
+          console.log(photoURL);
+          console.log(isAnonymous);
+
+          console.log(uid);
+          console.log(providerData);
+
+          
+          // ...
+        } else {
+          // User is signed out.
+          // ...
+        }
+      });
+
 
     location.on("child_added", function(loc){
         //loc.forEach(function(childSnapshot) {
